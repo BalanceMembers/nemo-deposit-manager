8177b6ddbbe96799324cb69c44968cc60898acc6
hemi333 <71807433+hemi333@users.noreply.github.com>
2022-10-31 19:38:57 +0900

    add Balances - Update
    add Balances - Update

--- routes/balance.js
+++ routes/balance.js
@@ -1,16 +1,66 @@
 const express = require("express");
 const router = express.Router();
-const { Balances } = require("../models");
+const { Balances, Clients } = require("../models");
 
-router.get("/", async (req, res) => {
+// 특정 클라이언트 적립금 내역 보기
+router.get("/:clientId", async (req, res) => {
   try {
-    const balances = await Balances.findAll({ order: [["no", "ASC"]] });
-    res.json({ balances });
+    const { clientId } = req.params;
+    const data = await Balances.findAll({ where: { clientId }, order: [["balanceId", "ASC"]] });
+    res.json({ success: true, data });
   } catch (error) {
     console.log(`${req.method} ${req.originalUrl} : ${error.message}`);
-    return res.status(400).json({
-      errorMessage: "게시글 조회에 실패하였습니다.",
-    });
+    return res.status(400).json({ success: false, errorMessage: "적립금 내역 조회에 실패하였습니다." });
+  }
+});
+
+// 특정 클라이언트 적립금 내역 추가
+router.post("/:clientId", async (req, res) => {
+  try {
+    const { clientId } = req.params;
+    const { date, content, in_charge, amount } = req.body;
+
+    const detailClient = await Clients.findOne({ where: { clientId } });
+
+    if (!detailClient) {
+      return res.status(404).json({ success: false, errorMessage: "해당 클라이언트가 없습니다." });
+    }
+
+    if (!date || !content || !in_charge || !amount) {
+      res.status(400).json({ success: false, message: "누락된 항목이 있습니다." });
+      return;
+    }
+
+    await Balances.create({ date, content, in_charge, amount });
+    res.status(201).json({ success: true, message: "적립금 내역을 추가하였습니다." });
+  } catch (error) {
+    console.log(`${req.method} ${req.originalUrl} : ${error.message}`);
+    return res.status(400).json({ success: false, errorMessage: "적립금 내역 추가에 실패하였습니다." });
+  }
+});
+
+// 특정 ID의 적립금내역 수정
+router.put("/:balanceId", async (req, res) => {
+  try {
+    const { balanceId } = req.params;
+    const { date, content, in_charge, amount } = req.body;
+
+    const detailBalance = await Balances.findOne({ where: { balanceId } });
+
+    if (!detailBalance) {
+      return res.status(404).json({ success: false, errorMessage: "해당 내역이 없습니다." });
+    }
+
+    if (!date || !content || !in_charge || !amount) {
+      res.status(400).json({ success: false, message: "누락된 항목이 있습니다." });
+      return;
+    }
+
+    await Balances.update({ date: date, content: content, in_charge: in_charge, amount: amount }, { where: { balanceId } });
+    res.status(201).json({ success: true, message: "적립금 내역을 수정하였습니다." });
+  } catch (error) {
+    console.log(`${req.method} ${req.originalUrl} : ${error.message}`);
+    return res.status(400).json({ success: false, errorMessage: "적립금 내역 수정에 실패하였습니다." });
   }
 });
 
--- routes/clients.js
+++ routes/clients.js
@@ -5,13 +5,11 @@ const { Clients } = require("../models");
 // 고객사 조회
 router.get("/", async (req, res) => {
   try {
-    const clients = await Clients.findAll({ order: [["clientId", "ASC"]] });
-    res.json({ clients });
+    const data = await Clients.findAll({ order: [["clientId", "ASC"]] });
+    res.json({ success: true, data });
   } catch (error) {
     console.log(`${req.method} ${req.originalUrl} : ${error.message}`);
-    return res.status(400).json({
-      errorMessage: "클라이언트 조회에 실패하였습니다.",
-    });
+    return res.status(400).json({ success: false, errorMessage: "클라이언트 조회에 실패하였습니다." });
   }
 });
 
@@ -19,19 +17,17 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     const { client, accessNo } = req.body;
-    await Clients.create({ client, accessNo });
 
     if (!client || !accessNo) {
-      res.status(400).json({ message: "누락된 항목이 있습니다." });
+      res.status(400).json({ success: false, message: "누락된 항목이 있습니다." });
       return;
     }
 
+    await Clients.create({ client, accessNo });
     res.status(201).json({ success: true, message: "클라이언트가 생성되었습니다." });
   } catch (error) {
     console.log(`${req.method} ${req.originalUrl} : ${error.message}`);
-    return res.status(400).json({
-      errorMessage: "클라이언트 생성에 실패하였습니다.",
-    });
+    return res.status(400).json({ success: false, errorMessage: "클라이언트 생성에 실패하였습니다." });
   }
 });
 
@@ -50,9 +46,7 @@ router.put("/:clientId", async (req, res) => {
     res.status(200).json({ success: true, message: "클라이언트를 수정하였습니다." });
   } catch (error) {
     console.log(`${req.method} ${req.originalUrl} : ${error.message}`);
-    return res.status(400).json({
-      errorMessage: "클라이언트 수정에 실패하였습니다.",
-    });
+    return res.status(400).json({ success: false, errorMessage: "클라이언트 수정에 실패하였습니다." });
   }
 });
 
@@ -70,9 +64,7 @@ router.delete("/:clientId", async (req, res) => {
     res.status(200).json({ success: true, message: "클라이언트를 삭제하였습니다." });
   } catch (error) {
     console.log(`${req.method} ${req.originalUrl} : ${error.message}`);
-    return res.status(400).json({
-      errorMessage: "클라이언트 삭제에 실패하였습니다.",
-    });
+    return res.status(400).json({ success: false, errorMessage: "클라이언트 삭제에 실패하였습니다." });
   }
 });
 
