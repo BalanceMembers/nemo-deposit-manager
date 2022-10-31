e56dff09615ae71da21622710cc2218d6312c54b
hemi333 <71807433+hemi333@users.noreply.github.com>
2022-10-31 17:28:34 +0900

    add Clients, Balances - Read
    add Clients, Balances - Read

--- routes/balance.js
+++ routes/balance.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const router = express.Router();
-// const { Balance } = require("../models");
+const { Balances } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const balances = await Balances.findAll({ order: [["no", "ASC"]] });
+    res.json({ balances });
+  } catch (error) {
+    console.log(`${req.method} ${req.originalUrl} : ${error.message}`);
+    return res.status(400).json({
+      errorMessage: "게시글 조회에 실패하였습니다.",
+    });
+  }
+});
 
 module.exports = router;
