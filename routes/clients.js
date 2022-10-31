bfdfa2e259e566e488fd010e50940900ee2099f6
hemi333 <71807433+hemi333@users.noreply.github.com>
2022-10-31 18:03:55 +0900

    add Clients - Update
    add Clients - Update

--- routes/clients.js
+++ routes/clients.js
@@ -5,7 +5,7 @@ const { Clients } = require("../models");
 // 모든 고객사 리스트 & 정보 보여주기
 router.get("/", async (req, res) => {
   try {
-    const clients = await Clients.findAll({ order: [["no", "ASC"]] });
+    const clients = await Clients.findAll({ order: [["clientId", "ASC"]] });
     res.json({ clients });
   } catch (error) {
     console.log(`${req.method} ${req.originalUrl} : ${error.message}`);
@@ -15,7 +15,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-// 고객사 새로 생성
+// 고객사 생성
 router.post("/", async (req, res) => {
   try {
     const { client, accessNo } = req.body;
@@ -31,6 +31,27 @@ router.post("/", async (req, res) => {
     console.log(`${req.method} ${req.originalUrl} : ${error.message}`);
     return res.status(400).json({
       errorMessage: "클라이언트 생성에 실패하였습니다.",
+    });
+  }
+});
+
+// 고객사 수정
+router.put("/:clientId", async (req, res) => {
+  try {
+    const { clientId } = req.params;
+    const { client, accessNo } = req.body;
+    const detailClient = await Clients.findOne({ where: { clientId } });
+
+    if (!detailClient) {
+      return res.status(404).json({ success: false, errorMessage: "해당 클라이언트가 없습니다." });
+    }
+
+    await Clients.update({ client: client, accessNo: accessNo }, { where: { clientId } });
+    res.status(200).json({ success: true, message: "클라이언트를 수정하였습니다." });
+  } catch (error) {
+    console.log(`${req.method} ${req.originalUrl} : ${error.message}`);
+    return res.status(400).json({
+      errorMessage: "게시글 수정에 실패하였습니다.",
     });
   }
 });
